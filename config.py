2cb02794e6c2f9fad1060cc9bdaa8080a0b0964f
Pavan Jadhav <k8574426@gmail.com>
2025-01-22 08:16:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ API_ID = int(environ.get("API_ID", "22759022"))
 API_HASH = environ.get("API_HASH", "32d849219dff492d2c3632cffa0bfb85")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
-PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split() # Bot Start Picture
+PICS = (environ.get('PICS', '')).split() # Bot Start Picture
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '873782213').split()]
 BOT_USERNAME = environ.get("BOT_USERNAME", "AKCRDbot") # without @
 PORT = environ.get("PORT", "8080")
@@ -53,7 +53,7 @@ CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", f"{script.CAPTION}")
 BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", CUSTOM_FILE_CAPTION)
 
 # Enable - True or Disable - False
-PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "False")), False)
+PUBLIC_FILE_STORE = is_disable((environ.get('PUBLIC_FILE_STORE', "False")), False)
 
 # Verify Info :-
 VERIFY_MODE = bool(environ.get('VERIFY_MODE', False)) # Set True or False
